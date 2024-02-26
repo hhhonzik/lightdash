77e7bf52c5087dd06a6b1cfab3ad755d7c155480
Honza Stepanovsky <jstepanovsky@id5.io>
2024-02-26 10:00:18 +0100

    fix DB and cli deployment

--- packages/common/src/types/projects.ts
+++ packages/common/src/types/projects.ts
@@ -123,10 +123,11 @@ export type CreateStarrocksCredentials = {
     type: WarehouseTypes.STARROCKS;
     host: string;
     user: string;
-    password: string;
+    password?: string;
     requireUserCredentials?: boolean;
     port: number;
-    dbname: string;
+    schema: string;
+    catalog: string;
     startOfWeek?: WeekDay | null;
 };
 export type StarrocksCredentials = Omit<
